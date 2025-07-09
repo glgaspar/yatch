ab6168fb0c2dced2719ba5879c8a87b2396a9682
Gustavo Gaspar <glgaspar@live.com>
2025-07-09 20:36:40 -0300

    getting requests to work

--- utils.py
+++ utils.py
@@ -1,26 +1,27 @@
+import sys
 import requests
 import os
 import json
 
-def api(method:str, path, data=None)->dict:
-    url = f"{os.getenv("API_URL")}{path}"
+def api(method:str, path, data={})->dict:
+    url = os.getenv('API_URL')+path
+    
     params = {
         "key": os.getenv("API_KEY"),
         "token": os.getenv("API_TOKEN")
     }
-    response = requests.request(method, url, params=params, data=data)
     
     try:
-        response = requests.get(url)
+        response = requests.request(method, url, params=params, data=data)
         response.raise_for_status()
     except requests.exceptions.RequestException as e:
         print(f"Error making request: {e}")
-        return dict()
+        return sys.exit()
 
     try:
         json_data = response.json()
     except json.JSONDecodeError as e:
         print(f"Error decoding JSON: {e}")
-        return dict()
+        return sys.exit()
     
     return json_data
